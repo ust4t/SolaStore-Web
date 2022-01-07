9c3efd61b0c54cb5d2701ca9e40702aeacedcc89
Ömer Ayhan <om.ayhan247@gmail.com>
2022-01-07 16:26:32 +0300

    :sparkles: test: - testing zifi package

--- src/layout/Stories/index.js
+++ src/layout/Stories/index.js
@@ -1,91 +1,70 @@
-import { Row } from "antd";
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation } from "swiper";
-import BrandSlider from "../../components/sliders/BrandSlider";
-
+import Story from "zifi";
+import { Component } from "react";
 import sources from "../../../sources";
-function Stories() {
-  return (
-    // <div className="select-colors  d-flex align-items-center justify-content-center">
-    //   <Swiper
-    //     style={{
-    //       padding: "0 60px",
-    //     }}
-    //     modules={[Autoplay, Navigation]}
-    //     spaceBetween={0}
-    //     centeredSlides={true}
-    //     slidesPerView={6}
-    //     // navigation
-    //     autoplay={{
-    //       delay: 6000,
-    //     }}
-    //     breakpoints={{
-    //       320: {
-    //         slidesPerView: 3,
-    //       },
-    //       768: {
-    //         slidesPerView: 3,
-    //       },
-    //       1024: {
-    //         slidesPerView: 6,
-    //       },
-    //     }}>
-    //     {[1, 2, 3, 4, 5, 6].map((variant, i) => (
-    //       <SwiperSlide>
-    //         <div
-    //           className="story-circle"
-    //           style={{
-    //             width: "95px",
-    //             height: "95px",
-    //             cursor: "pointer",
-    //             margin: "10px",
-    //           }}>
-    //           <img
-    //             key={`${i}__`}
-    //             className="color-select "
-    //             width={80}
-    //             height={80}
-    //             src={`${sources.imageMinSrc}ae56bd13-d.jpg`}
-    //             priority={true}
-    //           />
-    //         </div>
-    //       </SwiperSlide>
-    //     ))}
-    //   </Swiper>
-    // </div>
-    <div className="w-100">
-      <div
-        style={{
-          width: "400px",
-          marginLeft: "auto",
-          marginRight: "auto",
-        }}>
-        <Swiper
-          modules={[Autoplay, Navigation]}
-          spaceBetween={0}
-          centeredSlides={true}
-          slidesPerView={6}
-          // navigation
-          autoplay={{
-            delay: 6000,
-          }}>
-          {[1, 2, 3, 4, 5, 6].map((variant, i) => (
-            <SwiperSlide>
-              <img
-                key={`${i}__`}
-                className="color-select"
-                width={60}
-                height={60}
-                src={`${sources.imageMinSrc}ae56bd13-d.jpg`}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </div>
-    // <BrandSlider customPadding="pt-70" noBg />
-  );
+
+class StoriesSection extends Component {
+  render() {
+    return (
+      <Story fullScreen={true}>
+        <Story.Trigger>
+          <div>
+            <h4>2018's Most Innovative Companies</h4>
+            <img
+              alt="Tim Cook"
+              src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTE5NDg0MDU1MzM0OTc5MDg3/tim-cook-20967297-1-402.jpg"
+              height="100px"
+              width="100px"
+            />
+          </div>
+        </Story.Trigger>
+        <Story.Item
+          backgroundImage="url(https://pi.tedcdn.com/r/tedideas.files.wordpress.com/2017/03/frugal_innovation.png)"
+          blur="10px">
+          <div>
+            <h1>
+              2018's Most Innovative Companies are here{" "}
+              <span role="img">🌍</span> <span role="img">🚀</span>
+            </h1>
+            <p>
+              Fast Company just released their picks for the 350+ most
+              innovative companies of the year.
+            </p>
+          </div>
+        </Story.Item>
+        <Story.Item backgroundImage="linear-gradient(135deg, #fad961 0%,#f76b1c 100%)">
+          <div>
+            <p>
+              <strong>Fast Company </strong>
+              <span>Editorial Staff</span>
+            </p>
+          </div>
+          <blockquote>
+            The 2018 edition spans more than 350 enterprises across 35
+            categories, from the worlds most valuable firm to a small outfit
+            selling natural gum to preserve rainforests.
+          </blockquote>
+        </Story.Item>
+        <Story.Item>
+          <div>
+            <img src={`${sources.imageMinSrc}ae56bd13-d.jpg`} alt="Tim Cook" />
+            <p>
+              Apple won the coveted #1 spot, with its release of the much-loved
+              iPhone X, AirPod, and ARKit platform
+            </p>
+          </div>
+        </Story.Item>
+        <Story.Item>
+          <p>
+            A surprising contender, Patagonia came in sixth in part for their
+            focus on helping the environment
+          </p>
+        </Story.Item>
+        <Story.Item backgroundImage="linear-gradient(135deg, #5b247a 0%,#1bcedf 100%);">
+          <div>Read More</div>
+        </Story.Item>
+      </Story>
+    );
+  }
 }
 
-export default Stories;
+export default StoriesSection;
