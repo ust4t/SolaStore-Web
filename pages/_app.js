25b94ad5b303fa6878409e07438a06899d8432e1
omer_ayhan <om.ayhan247@gmail.com>
2022-01-27 19:17:30 +0300

    perf: :zap: minified css

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,7 @@ import 'swiper/css/bundle';
 import 'animate.css';
 import 'antd/dist/antd.css';
 import '../styles/global.css';
+import 'react-input-range/lib/css/index.css';
 
 function MyApp({ Component, pageProps }) {
 	const router = useRouter();
@@ -173,7 +174,12 @@ function MyApp({ Component, pageProps }) {
 					content='/img/logo/favicon/ms-icon-144x144.png'
 				/>
 				<meta name='theme-color' content='#ffffff' />
-
+				<link
+					href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css'
+					rel='stylesheet'
+					integrity='sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl'
+					crossorigin='anonymous'
+				/>
 				<link
 					href='https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
 					rel='stylesheet'
@@ -193,6 +199,7 @@ function MyApp({ Component, pageProps }) {
 			<Script
 				src='https://kit.fontawesome.com/9134714f20.js'
 				crossorigin='anonymous'
+				defer
 			/>
 		</Provider>
 	);
