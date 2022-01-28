7d507bd3a4f649e3322307cc07e11fe7cdf599c9
omer_ayhan <om.ayhan247@gmail.com>
2022-01-28 12:06:57 +0300

    fix: :bug: server configuration

--- server.js
+++ server.js
@@ -3,25 +3,39 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-	createServer((req, res) => {
-		// Be sure to pass `true` as the second argument to `url.parse`.
-		// This tells it to parse the query portion of the URL.
-		const parsedUrl = parse(req.url, true);
-		const { pathname, query } = parsedUrl;
+	const server = createServer();
+	// createServer((req, res) => {
+	// 	// Be sure to pass `true` as the second argument to `url.parse`.
+	// 	// This tells it to parse the query portion of the URL.
+	// 	const parsedUrl = parse(req.url, true);
+	// 	const { pathname, query } = parsedUrl;
 
-		if (pathname === '/a') {
-			app.render(req, res, '/a', query);
-		} else if (pathname === '/b') {
-			app.render(req, res, '/b', query);
-		} else {
-			handle(req, res, parsedUrl);
+	// 	if (pathname === '/a') {
+	// 		app.render(req, res, '/a', query);
+	// 	} else if (pathname === '/b') {
+	// 		app.render(req, res, '/b', query);
+	// 	} else {
+	// 		handle(req, res, parsedUrl);
+	// 	}
+	// }).listen(process.env.PORT, (err) => {
+	// 	if (err) throw err;
+	// 	console.log('> Ready on http://localhost:3000');
+	// });
+
+	server.all('*', (req, res) => {
+		return handle(req, res);
+	});
+
+	server.listen(port, (err) => {
+		if (err) {
+			console.error(err);
+			throw err;
 		}
-	}).listen(process.env.PORT, (err) => {
-		if (err) throw err;
-		console.log('> Ready on http://localhost:3000');
+		console.log(`> Ready on http://localhost:${port}`);
 	});
 });
