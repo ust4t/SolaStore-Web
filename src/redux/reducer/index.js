d81496b7ffed8dd065cb92c9405bd6d5e035a878
omer_ayhan <om.ayhan247@gmail.com>
2022-01-28 16:38:19 +0300

    perf: :fire: removed unused js files

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,30 +1,28 @@
-import { combineReducers } from "redux";
-import blog from "./blog";
-import features from "./features";
-import filter from "./filter";
-import home from "./home";
-import product from "./product";
-import seller from "./seller";
-import upcomingProduct from "./upcomingProduct";
-import utilis from "./utilis";
-import populars from "./populars";
-import menu from "./menu";
-import lang from "./lang";
-import brands from "./brands";
-import auth from "./auth";
+import { combineReducers } from 'redux';
+import features from './features';
+import filter from './filter';
+import home from './home';
+import product from './product';
+import seller from './seller';
+import upcomingProduct from './upcomingProduct';
+import utilis from './utilis';
+import populars from './populars';
+import menu from './menu';
+import lang from './lang';
+import brands from './brands';
+import auth from './auth';
 
 export default combineReducers({
-  blog,
-  lang,
-  seller,
-  features,
-  upcomingProduct,
-  product,
-  brands,
-  filter,
-  home,
-  utilis,
-  populars,
-  menu,
-  auth,
+	lang,
+	seller,
+	features,
+	upcomingProduct,
+	product,
+	brands,
+	filter,
+	home,
+	utilis,
+	populars,
+	menu,
+	auth,
 });
