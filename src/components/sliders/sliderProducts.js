5a4ac2b8b4b209120d1ac53bc332f19de520ebbd
omer_ayhan <om.ayhan247@gmail.com>
2022-01-31 10:44:07 +0300

    perf: :zap: homepage slider style update

--- src/components/sliders/sliderProducts.js
+++ src/components/sliders/sliderProducts.js
@@ -19,7 +19,7 @@ export default function SliderProducts({ sliders }) {
     <section className="hero-area position-relative ">
       <div className="hero-slider-two">
         <div className="slider-active slider-active-one">
-          <HomePageSliderWithArrow>
+          <HomePageSliderWithArrow sliders={sliders}>
             {sliders &&
               sliders.map(
                 (
@@ -32,46 +32,84 @@ export default function SliderProducts({ sliders }) {
                   },
                   i
                 ) => (
+                  // <div
+                  //   key={`${pictureID}_-_${i}`}
+                  //   className={`single-slider ${slider_height} d-flex align-items-end bg-center-cover`}>
+                  //   <div className="container">
+                  //     <div className="row">
+                  //       <div className="col-lg-12 p-0">
+                  //         <div
+                  //           className={`${hero_caption} ${hero_container} position-relative mb-85 d-flex flex-column align-items-center justify-content-center`}>
+                  //           <BackgroundImage
+                  //             src={`${sources.slider}${guidName}`}
+                  //             objectFit="cover"
+                  //             objectPosition="center"
+                  //             layout="fill"
+                  //             quality={100}
+                  //             priority={true}>
+                  //             <h5
+                  //               className="text-white fadeInUp wow text-uppercase text-shadow"
+                  //               data-animation="fadeInUp"
+                  //               data-delay=".2s">
+                  //               {selectedText1}
+                  //             </h5>
+                  //             <h2
+                  //               className="text-white fadeInUp wow text-uppercase text-shadow"
+                  //               data-animation="fadeInUp"
+                  //               data-delay=".4s">
+                  //               {selectedText2}
+                  //             </h2>
+                  //             <Link
+                  //               href={
+                  //                 selectedText1 === "İNDİRİMLİ ÜRÜNLER"
+                  //                   ? "/shop/saleproducts"
+                  //                   : "/shop/newproducts"
+                  //               }>
+                  //               <a
+                  //                 className={`${sliderButton} w-25`}
+                  //                 rel="noopener noreferrer">
+                  //                 {selectedTextButton}
+                  //               </a>
+                  //             </Link>
+                  //           </BackgroundImage>
+                  //         </div>
+                  //       </div>
+                  //     </div>
+                  //   </div>
+                  // </div>
                   <div
                     key={`${pictureID}_-_${i}`}
-                    className={`single-slider ${slider_height} d-flex align-items-end bg-center-cover`}>
+                    className={`single-slider ${slider_height} d-flex align-items-end bg-center-cover`}
+                    data-background={`${sources.slider}${guidName}`}>
                     <div className="container">
                       <div className="row">
-                        <div className="col-lg-12 p-0">
+                        <div className="col-lg-12">
                           <div
                             className={`${hero_caption} ${hero_container} position-relative mb-85 d-flex flex-column align-items-center justify-content-center`}>
-                            <BackgroundImage
-                              src={`${sources.slider}${guidName}`}
-                              objectFit="cover"
-                              objectPosition="center"
-                              layout="fill"
-                              quality={100}
-                              priority={true}>
-                              <h5
-                                className="text-white fadeInUp wow text-uppercase text-shadow"
-                                data-animation="fadeInUp"
-                                data-delay=".2s">
-                                {selectedText1}
-                              </h5>
-                              <h2
-                                className="text-white fadeInUp wow text-uppercase text-shadow"
-                                data-animation="fadeInUp"
-                                data-delay=".4s">
-                                {selectedText2}
-                              </h2>
-                              <Link
-                                href={
-                                  selectedText1 === "İNDİRİMLİ ÜRÜNLER"
-                                    ? "/shop/saleproducts"
-                                    : "/shop/newproducts"
-                                }>
-                                <a
-                                  className={`${sliderButton} w-25`}
-                                  rel="noopener noreferrer">
-                                  {selectedTextButton}
-                                </a>
-                              </Link>
-                            </BackgroundImage>
+                            <h5
+                              className="text-white fadeInUp wow text-uppercase text-shadow"
+                              data-animation="fadeInUp"
+                              data-delay=".2s">
+                              {selectedText1}
+                            </h5>
+                            <h2
+                              className="text-white fadeInUp wow text-uppercase text-shadow"
+                              data-animation="fadeInUp"
+                              data-delay=".4s">
+                              {selectedText2}
+                            </h2>
+                            <Link
+                              href={
+                                selectedText1 === "İNDİRİMLİ ÜRÜNLER"
+                                  ? "/shop/saleproducts"
+                                  : "/shop/newproducts"
+                              }>
+                              <a
+                                className={`${sliderButton} w-25`}
+                                rel="noopener noreferrer">
+                                {selectedTextButton}
+                              </a>
+                            </Link>
                           </div>
                         </div>
                       </div>
