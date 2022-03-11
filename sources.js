907cce8d585d5af9f660ae318b2b7c91d7624565
omer_ayhan <om.ayhan247@gmail.com>
2022-03-11 17:38:53 +0300

    fix: :fire: fix for slider

--- sources.js
+++ sources.js
@@ -3,7 +3,7 @@ export default {
   imageMidSrc: "https://solastore.com.tr/img/ProductWM/midPic/",
   imageMinSrc: "https://solastore.com.tr/img/ProductWM/minPic/",
   videos: "https://solastore.com.tr/videos/Product/",
-  slider: "https://solastore.com.tr/img/Slider/",
+  slider: "https://solastore.com.tr/img/slider/",
   banners: "https://solastore.com.tr//img/MainAdd/",
   campaign: "https://solastore.com.tr/img/MainCampaingn/",
   brand: "https://solastore.com.tr/img/Brand/",
