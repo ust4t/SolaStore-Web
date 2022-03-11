53aab3f469da4c0c104b0ad500dd1caa3939f3bb
omer_ayhan <om.ayhan247@gmail.com>
2022-03-11 15:52:01 +0300

    fix: :fire: fix for wheel api

--- pages/api/advertisement/generateCoupon.js
+++ pages/api/advertisement/generateCoupon.js
@@ -1,11 +1,15 @@
 import axios from "axios";
+import requestIp from "request-ip";
 
 export default async function (req, res) {
+  const clientIp = requestIp.getClientIp(req);
   try {
     const { tel, giftId } = req.query;
 
     const { data } = await axios.get(
-      `https://api.solastore.com.tr/api/Advertising/GetPassionFlowerVoucher?phoneNumber=${tel}&giftVoucherID=${giftId}&sourceProof=${process.env.SOURCE_PROOF}`
+      `https://api.solastore.com.tr/api/Advertising/GetPassionFlowerVoucher?phoneNumber=${tel}&ip=${clientIp.toString()}&giftVoucherID=${giftId}&sourceProof=${
+        process.env.SOURCE_PROOF
+      }`
     );
 
     await res.status(200).json(data);
