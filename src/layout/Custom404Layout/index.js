41a8cb10907cbe6e29e7d970c43fab7744685ad8
omer_ayhan <om.ayhan247@gmail.com>
2022-03-15 11:11:42 +0300

    fix: :bug: translation for 404 page

--- src/layout/Custom404Layout/index.js
+++ src/layout/Custom404Layout/index.js
@@ -1,3 +1,5 @@
+import useTranslation from "next-translate/useTranslation";
+
 import Layout from "../Layout";
 import {
   container,
@@ -8,6 +10,7 @@ import {
 } from "./Custom404.module.css";
 
 export default function Custom404Layout() {
+  const { t } = useTranslation("common");
   return (
     <Layout news={4} logoLeft layout={2} paymentOption>
       <div className={container}>
@@ -16,7 +19,7 @@ export default function Custom404Layout() {
           <div className={textContainer}>
             <h1 className={`text-center fw-bold text-white ${title}`}>404</h1>
             <h2 className="text-center fw-bold fs-2 text-white">
-              Page Not Found
+              {t("title404")}
             </h2>
           </div>
         </div>
