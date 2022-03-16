9a506e2b14cf75fbd656f28a15dd63eef1a9ecae
omer_ayhan <om.ayhan247@gmail.com>
2022-03-16 17:47:00 +0300

    :bug: fix: fixed import issue in Details component

---
+++ src/components/sliders/SliderArrows.js
@@ -0,0 +1,11 @@
+export const Arrow = ({ icon, onClick, className, text }) => {
+  const ClickEvent = () => {
+    onClick();
+  };
+  return (
+    <button type="button" className={className} onClick={ClickEvent}>
+      <i className={icon} />
+      {text && <span>{text}</span>}
+    </button>
+  );
+};
