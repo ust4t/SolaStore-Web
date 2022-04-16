0d9b1a91f669f331d0ad6caa99c55aeac746b486
omer_ayhan <om.ayhan247@gmail.com>
2022-04-16 11:36:34 +0300

    fix: :zap: turned car gif into a lottie component

--- src/components/VisitStore/VisitStore.js
+++ src/components/VisitStore/VisitStore.js
@@ -1,12 +1,25 @@
+import React from "react";
+import { useLottie } from "lottie-react";
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
-import React from "react";
-import sources from "../../../sources";
 
+import Car from "../../../public/lottie/car.json";
 import { container, buttonStore, visitTitle } from "./VisitStore.module.css";
 
 export default function VisitStore() {
   const { t } = useTranslation("home");
+  const options = {
+    animationData: Car,
+    loop: true,
+    autoplay: true,
+    style: {
+      width: "350px",
+      height: "350px",
+      margin: "-70px",
+    },
+  };
+
+  const { View } = useLottie(options);
 
   return (
     <div
@@ -17,7 +30,7 @@ export default function VisitStore() {
           display: "grid",
           placeContent: "center",
         }}>
-        <img className="img-fluid" src="/images/car.gif" alt="" />
+        {View}
       </div>
       <div className="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-center">
         <h1 className={`text-center ${visitTitle} fw-bold`}>
